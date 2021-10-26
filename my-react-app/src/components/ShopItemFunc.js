75e99c71f7674c1ce7307ad63627aa52603f434b
AlexAdov <89395875+AlexAdov@users.noreply.github.com>
2021-10-26 11:33:43 +0300

    step3

--- my-react-app/src/components/ShopItemFunc.js
+++ my-react-app/src/components/ShopItemFunc.js
@@ -7,27 +7,28 @@ import ShopModel from '../models/ShopModel';
 function ShopItemFunc(props) {
   const {item} = props;
 
-return (
-  <div>
-    <div class="main-content">
-      <h2>{item.brand}</h2>
-      <h1>{item.title}</h1>
-      <h3>{item.description}</h3>
-      <div class="description">{item.descriptionFull}</div>
-      <div class="highlight-window mobile">
-        <div class="highlight-overlay"></div>
-      </div>
-      <div class="divider"></div>
-      <div class="purchase-info">
-        <div class="price">{item.currency}{item.price}</div>
-        <button>Добавить в корзину</button>
+  return (
+    <div>
+      <div class="main-content">
+        <h2>{item.brand}</h2>
+        <h1>{item.title}</h1>
+        <h3>{item.description}</h3>
+        <div class="description">{item.descriptionFull}</div>
+        <div class="highlight-window mobile">
+          <div class="highlight-overlay"></div>
+        </div>
+        <div class="divider"></div>
+        <div class="purchase-info">
+          <div class="price">{item.currency}{item.price}</div>
+          <button>Добавить в корзину</button>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
+
 ShopItemFunc.propTypes = {
   item: PropTypes.instanceOf(ShopModel).isRequired
 }
 
-  export default ShopItemFunc;
+export default ShopItemFunc;
