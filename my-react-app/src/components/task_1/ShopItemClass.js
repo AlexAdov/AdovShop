46c82076f57fef6dafeb2ae190ad1f01aca9d9b7
AlexAdov <89395875+AlexAdov@users.noreply.github.com>
2021-11-02 19:46:12 +0300

    Tasks

--- my-react-app/src/components/task_1/ShopItemClass.js
+++ my-react-app/src/components/task_1/ShopItemClass.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import ShopModel from '../models/ShopModel';
+import ShopModel from '../../models/ShopModel';
 
 
 class ShopItemClass extends React.Component {
--- my-react-app/src/components/task_1/ShopItemFunc.js
+++ my-react-app/src/components/task_1/ShopItemFunc.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import ShopModel from '../models/ShopModel';
+import ShopModel from '../../models/ShopModel';
 
 
 function ShopItemFunc(props) {
