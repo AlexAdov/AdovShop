547edb9dbfeae64538c2b7e1c1253e8bc76c0705
AlexAdov <89395875+AlexAdov@users.noreply.github.com>
2021-11-03 12:25:22 +0300

    button

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ShopItemFunc from "./components/task_1/ShopItemFunc";
 import ShopItemClass from './components/task_1/ShopItemClass';
 import './App.css';
 import Task_1 from './components/task_1';
+import Task_2 from './components/task_2';
 
 function App() {
+  const [task, setTask] = useState("task2");
+
+  const handleClick = (e) => {
+    const task = e.currentTarget.dataset.name
+    setTask(task)
+  }
 
   return (
     <div>
+      <nav>
+        <button data-name="task1" onClick={handleClick}>Урок 1</button>
+        <button data-name="task2" onClick={handleClick}>Урок 2</button>
+      </nav>
 
-      <Task_1 />
+      {(() => {
+        switch (task) {
+          case 'task1':
+            return <Task_1 />;
+          case 'task2':
+            return <Task_2 />;
+          default:
+            return null;
+        }
+      })()}
+
 
     </div>
   );
--- my-react-app/src/components/task_1/index.js
+++ my-react-app/src/components/task_1/index.js
@@ -22,16 +22,11 @@ function Task_1() {
         <div className="window">
           <ShopItemClass item={item} />
         </div>
-      </div>
-      <div className="container">
-        <div className="background-element"></div>
-        <div className="highlight-window">
-          <div className='highlight-overlay'></div>
-        </div>
         <div className="window">
           <ShopItemFunc item={item} />
         </div>
       </div>
+
     </div>
   );
 }
