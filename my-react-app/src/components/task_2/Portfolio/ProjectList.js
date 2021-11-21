afdc52ec228876eca9de4e8a2ba68d5ed89e5252
AlexAdov <89395875+AlexAdov@users.noreply.github.com>
2021-11-22 01:46:53 +0300

    ArrNull

--- my-react-app/src/components/task_2/Portfolio/ProjectList.js
+++ my-react-app/src/components/task_2/Portfolio/ProjectList.js
@@ -2,24 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export function ProjectList(props) {
-  return (
-    <div className="portfolio_grid">
-      {props.projects.map((project) => (
-        <img
-          key={project.index}
-          className="portfolio_item"
-          src={project.img}
-        />
-      ))}
-    </div>
-  );
+   return (
+      <div className="portfolio_grid">
+         {props.projects && props.projects.length ? props.projects.map((project) => (
+                 <img
+                    key={project.index}
+                    className="portfolio_item"
+                    src={project.img}
+                 />
+              ))
+            : null}
+      </div>
+   );
 }
 
-
-
 ProjectList.propTypes = {
-  projects: PropTypes.array.isRequired,
+   projects: PropTypes.array.isRequired,
 };
 ProjectList.defaultProps = {
-   src: 'https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg',
- };
+   src: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+};
