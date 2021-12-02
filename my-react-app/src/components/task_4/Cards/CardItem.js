8fc91636bd9756cb73b348d6bda9b67e300a384e
AlexAdov <89395875+AlexAdov@users.noreply.github.com>
2021-12-03 01:00:36 +0300

    task4

--- my-react-app/src/components/task_4/Cards/CardItem.js
+++ my-react-app/src/components/task_4/Cards/CardItem.js
@@ -1,42 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import { Card, Button } from "react-bootstrap";
 
+function CardItem(props) {
+   const handleButtonClick = (e) => {
+      e.preventDefault();
+      props.onButtonClick(e.target);
+   };
 
-
-function CardItem({ image, children, title }) {
-
-  const handleButtonClick = (evt) => {
-    evt.preventDefault();
-    console.log(evt.children)
-  }
-
-  
-
-  return (
-
-<>
-    { image && <Card.Img variant="top" src={image} />}
-          <Card.Body>
-            {title}
-            <Card.Text>
-             {children}
-            </Card.Text>
-        <Button variant="primary" onClick={handleButtonClick}>Go somewhere</Button>
-          </Card.Body>
-
-
-
-</>
-    
-  );
+   return (
+      <>
+         <Card>
+         {props.children}
+            <Card.Body>
+               <Card.Title>{props.title}</Card.Title>
+               <Card.Text>{props.text}</Card.Text>
+               <Button variant="primary" onClick={handleButtonClick}>
+                  {props.buttonCaption}
+               </Button>
+            </Card.Body>
+         </Card>
+      </>
+   );
 }
 
 CardItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-  buttonCaption: PropTypes.string.isRequired,
-  onButtonClick: PropTypes.func.isRequired
-}
+   title: PropTypes.string.isRequired,
+   text: PropTypes.string.isRequired,
+   buttonCaption: PropTypes.string.isRequired,
+   onButtonClick: PropTypes.func.isRequired,
+};
 
 export default CardItem;
--- my-react-app/src/components/task_4/index.js
+++ my-react-app/src/components/task_4/index.js
@@ -2,34 +2,34 @@ import React, { useState } from "react";
 import { Cards } from "./Cards";
 import { Decomposition } from "./Decomposition";
 
-
 export function Task_4() {
-  const [task, setTask] = useState("cards");
+   const [task, setTask] = useState("cards");
 
-  const handleClick = (e) => {
-    const task = e.currentTarget.dataset.name;
-    setTask(task);
-  };
+   const handleClick = (e) => {
+      const task = e.currentTarget.dataset.name;
+      setTask(task);
+   };
 
-  
-  return (
-    <div>
-      <nav Style={"margin-top:45px;"}>
-        <button data-name="cards" onClick={handleClick}>Карточки</button>
-        <button data-name="decomposition" onClick={handleClick}>Декомпозиция</button>
-      </nav>
-        {(() => {
-          switch (task) {
-            case "cards":
-              return <Cards />;
-            case "decomposition":
-              return <Decomposition />;
-            default:
-              return null;
-          }
-        })()}
-    </div>
-  );
+   return (
+      <div>
+         <nav className="nav2">
+            <button data-name="cards" onClick={handleClick}>
+               Карточки
+            </button>
+            <button data-name="decomposition" onClick={handleClick}>
+               Декомпозиция
+            </button>
+         </nav>
+         {(() => {
+            switch (task) {
+               case "cards":
+                  return <Cards />;
+               case "decomposition":
+                  return <Decomposition />;
+               default:
+                  return null;
+            }
+         })()}
+      </div>
+   );
 }
-
- 
